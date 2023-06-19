cffb48253111bfe72f1f2acf4e5059c7d1935150
stoswebsolutions <stoswebsolutions@gmail.com>
2023-06-19 21:42:59 +0530

    Karya-UI_v4 imported

--- script.js
+++ script.js
@@ -8,8 +8,42 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
 
 // for range slider in pricing plans
 const rangeSlider = document.getElementById('sale-range');
-rangeSlider.oninput = function () {
-  $('.selected-value').html(this.value);
-  $('.profile-val').html(this.value * 10);
-  $('.income-val').html(this.value * 400);
-};
+if (rangeSlider) {
+  rangeSlider.oninput = function () {
+    $('.selected-value').html(this.value);
+    $('.profile-val').html(this.value * 10);
+    $('.income-val').html(this.value * 400);
+  };
+}
+
+// for menu
+// $('.dashboard .menu li').click(function () {
+$('.dashboard-item, .status, .exploreJobs, .exploreCompanies, .logout').click(
+  function () {
+    $('.dashboard .menu li').removeClass('active forDashboard');
+    const selectedItem = $(this).attr('name');
+    $('.menu-item-select option[value=' + selectedItem + ']').attr(
+      'selected',
+      'selected'
+    );
+    if ($(this).hasClass('dashboard-item')) {
+      $(this).addClass('active');
+      $(this).addClass('forDashboard');
+      return;
+    }
+    $(this).addClass('active');
+    return;
+  }
+);
+
+// for select menu
+$('.menu-item-select').on('change', function () {
+  $('.dashboard .menu li').removeClass('active forDashboard');
+  if (this.value === 'dashboard') {
+    $('.dashboard-item').addClass('active');
+    $('.dashboard-item').addClass('forDashboard');
+    return;
+  }
+  $('.' + this.value).addClass('active');
+  return;
+});
